1bfdc1576b13cf516a52afe6977dba7a36300bd2
UtkinVadim <utkinvadim1991@gmail.com>
2024-11-28 22:39:43 +0300

    Implement async auth check in PrivateRoutes component

--- packages/client/src/privateRoutes.tsx
+++ packages/client/src/privateRoutes.tsx
@@ -1,12 +1,40 @@
+import { useEffect, useState } from 'react';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
+
+import { Routes } from '@Constants/Routes';
+import { useAuthApi } from '@Services/AuthService';
+import type { IProfileDataState } from '@Store/Types/User.types';
 
 export const PrivateRoutes = () => {
   const location = useLocation();
-  const isAuth = true; //TODO: тут должна быть проверка авторизации
+  const authApi = useAuthApi();
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
 
-  return isAuth ? (
+  const checkAuth = async () => {
+    const userInfo: IProfileDataState | null = await authApi.getUserInfo();
+    return Boolean(userInfo);
+  };
+
+  // Использование хука useEffect для выполнения асинхронной проверки
+  useEffect(() => {
+    (async () => {
+      try {
+        const auth = await checkAuth();
+        setIsAuthenticated(auth);
+      } catch (error) {
+        setIsAuthenticated(false);
+      }
+    })();
+  }, []);
+
+  if (isAuthenticated === null) {
+    // альтернатива: отображать спиннер или загрузочный индикатор
+    return <div>Проверка авторизации...</div>;
+  }
+
+  return isAuthenticated ? (
     <Outlet />
   ) : (
-    <Navigate to='/sing-up' replace state={{ from: location }} />
+    <Navigate to={`/${Routes.SignIn}`} replace state={{ from: location }} />
   );
 };
