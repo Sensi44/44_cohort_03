3de590f598cbc1c7ee451cc9bd96ca979386f5c6
UtkinVadim <utkinvadim1991@gmail.com>
2024-11-30 15:53:13 +0300

    Enable logout functionality in LogoutButton component

--- packages/client/src/Components/LogoutButton/LogoutButton.tsx
+++ packages/client/src/Components/LogoutButton/LogoutButton.tsx
@@ -1,25 +1,21 @@
 import { Button } from '@mui/material';
 import React from 'react';
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-// import { Routes } from '@Constants';
-// import { useLogoutMutation } from '@Store';
+import { Routes } from '@Constants';
+import { useLogoutMutation } from '@Store';
 
 export const LogoutButton: React.FC = () => {
-  // const navigate = useNavigate();
-  // const [logout, { isLoading, isError }] = useLogoutMutation();
+  const navigate = useNavigate();
+  const [logout] = useLogoutMutation();
 
-  const handleLogout = () => {
-    console.log('logout');
-    // try {
-    //   logout()
-    //     .unwrap()
-    //     .then(() => {
-    //       navigate(`/${Routes.SignIn}`);
-    //     });
-    // } catch (error) {
-    //   return;
-    // }
+  const handleLogout = async () => {
+    try {
+      await logout().unwrap();
+      navigate(`/${Routes.SignIn}`);
+    } catch (error) {
+      console.error('Ошибка при выходе:', error);
+    }
   };
 
   return (
