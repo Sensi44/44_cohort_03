2476573e13c5cca63a4816a21cf99504bc96a691
Classified <mur.sensi@yandex.ru>
2024-12-06 14:22:12 +0300

    YP-21 fix: delete comments

--- packages/client/src/RenderFunctions/DrawPlayer.tsx
+++ packages/client/src/RenderFunctions/DrawPlayer.tsx
@@ -4,31 +4,8 @@ import type { TCanvas } from '@Types';
 const image = new Image();
 image.src = '../../src/Assets/img/beaver-plane.png';
 
-// const backgroundImage = new Image();
-// backgroundImage.src = '../../src/Assets/img/background.jpg';
-//
-// const backTemp = {
-//   x: 0,
-//   y: 0,
-// };
-//
-// export const drawBackground = (ctx: TCanvas) => {
-//   if (ctx) {
-//     ctx.drawImage(backgroundImage, backTemp.x, 0);
-//     ctx.drawImage(backgroundImage, backTemp.x + backgroundImage.width, 0);
-//   }
-//
-//   backTemp.x -= 1; // Двигаем фон влево
-//
-//   if (backTemp.x <= -backgroundImage.width) {
-//     backTemp.x = 0;
-//   }
-// };
-
 export const drawPlayer = (ctx: TCanvas, player: TPlayer) => {
   if (ctx) {
-    // drawBackground(ctx);
-
     ctx.drawImage(image, player.x, player.y, player.width, player.height);
   }
 };
