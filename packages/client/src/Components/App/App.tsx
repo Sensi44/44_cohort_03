03421c3ebefd154e45ce4ee7cf49461190cb0b9f
Alexander <oops.krit@yandex.ru>
2024-12-12 19:20:54 +0300

    feat: add gameIntro component

--- packages/client/src/Components/App/App.tsx
+++ packages/client/src/Components/App/App.tsx
@@ -6,6 +6,7 @@ import { DebugPanel, Menu } from '@Components';
 import { useRender, useUpdate } from '@Game';
 import { useGameLoop, useGetCanvasSize, useSetCanvasContext } from '@Hooks';
 
+import { GameIntro } from '../GameIntro/GameIntro';
 import './App.scss';
 
 export const App = () => {
@@ -54,6 +55,7 @@ export const App = () => {
             width={width}
             height={height}
           />
+          <GameIntro onStart={startGame} />
         </div>
       </section>
     </article>
